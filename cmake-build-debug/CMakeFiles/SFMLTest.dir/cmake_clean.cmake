623f701717dda0b283a4ed20b53ee0909f8fe9b2
Wafol <pavel.krauz1@gmail.com>
2022-05-18 20:53:47 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/SFMLTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFMLTest.dir/main.cpp.o"
+  "SFMLTest.exe"
+  "SFMLTest.pdb"
+  "libSFMLTest.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFMLTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
